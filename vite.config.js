3da791b6fbe99a9f14d0c79a57ed33c2dd70044b
Lucas <dev.lucas.fdantas@gmail.com>
2026-09-17 10:35:09 -0300

    Configurando deploy para o GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: './',
+  base: 'https://github.com/lucasfdantas/Leitor_de_OS_React',
   plugins: [react()],
 })
